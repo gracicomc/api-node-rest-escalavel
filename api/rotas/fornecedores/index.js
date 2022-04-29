b1d981b13a991583138174c73fc697b890ead2b7
Graciela Parente <graciparentte@gmail.com>
2022-04-29 17:52:48 -0300

    POST created

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,9 +1,19 @@
 const roteador = require("express").Router();
 const TabelaFornecedor = require("./TabelaFornecedor");
+const Fornecedor = require("./Fornecedor");
 
-roteador.use("/", async (requisicao, resposta) => {
+//listar fornecedores
+roteador.get("/", async (requisicao, resposta) => {
   const resultados = await TabelaFornecedor.listar();
   resposta.send(JSON.stringify(resultados));
+});
+
+//criar/inserir fornecedores
+roteador.post("/", async (requisicao, resposta) => {
+  const dadosRecebidos = requisicao.body;
+  const fornecedor = new Fornecedor(dadosRecebidos);
+  await fornecedor.criar();
+  resposta.send(JSON.stringify(fornecedor));
 });
 
 module.exports = roteador;
