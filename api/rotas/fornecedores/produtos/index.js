ababd9150b67efc07f74060f9ed3ea14880c5776
Graciela Parente <graciparentte@gmail.com>
2022-05-04 16:04:07 -0300

    feat: create sync between produtos and fornecedores

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,6 +1,9 @@
-const roteador = require("express").Router();
-roteador.get("/", (requisicao, resposta) => {
-  resposta.send(JSON.stringify([]));
+const roteador = require("express").Router({ mergeParams: true });
+const Tabela = require("./TabelaProduto");
+
+roteador.get("/", async (requisicao, resposta) => {
+  const produtos = await Tabela.listar(requisicao.params.idFornecedor);
+  resposta.send(JSON.stringify(produtos));
 });
 
 module.exports = roteador;
